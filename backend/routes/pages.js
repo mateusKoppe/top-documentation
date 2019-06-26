a41917e4b11f6fd3e6cadafef3d0823dce06b749
Mateus Koppe <mateus.koppe@gmail.com>
2019-06-26 17:35:14 -0300

    feat(pages): create list endpoint

--- backend/routes/pages.js
+++ backend/routes/pages.js
@@ -3,12 +3,17 @@ var router = express.Router();
 
 const Pages = require('../models/pages')
 
+router.get('/', (req, res, next) => {
+  const pages = Pages.getPageList();
+  res.json({ pages });
+})
+
 router.get('/:page', function(req, res, next) {
   const path = Pages.formatPath(req.params.page)
   const openPath = Pages.formatPath(req.query.open)
   const pages = Pages.getPageActiveList(openPath);
   const content = Pages.getPageContent(path);
-  res.render('pages/index', {
+  res.json({
     content,
     pages,
     title: req.params.page
